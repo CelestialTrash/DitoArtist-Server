93ea361a77ee738930dc93041c308db5742161d0
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-26 13:16:06 +0200

    product schema fix

--- models/Product.model.js
+++ models/Product.model.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema
 const productSchema = new Schema ({
     title: {type: String, required: true},
     price: {type: Number, required: true},
-    imageUrl: {type: String}
+    description: {type: String},
+    imageUrl: {type: [String]},
+    size: {type: String},
+    materials: {type: String}
 })
 
 const Product = mongoose.model("Product", productSchema)
